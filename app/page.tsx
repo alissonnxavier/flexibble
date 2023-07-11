3a11fc014d55000a3b9efea206b5e5eb8ddc2dc6
Alisson <alissonalves29@gmail.com>
2023-07-11 11:59:05 -0300

    start

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='flex-start flex-col paddings mb-16'>
+        <h1 className='text-green-500'>Categories</h1>
+        <h1>Posts</h1>
+        <h1>LoadMore</h1>
+    </section>
+  )
+}
+
+export default Home;
